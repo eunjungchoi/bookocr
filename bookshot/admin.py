867a25b9384e7f9640145dfdb74d241d5d3bfcba
eunjungchoi <im.your.energy@gmail.com>
2016-06-28 03:40:08 +0900

    templates sync

--- bookshot/admin.py
+++ bookshot/admin.py
@@ -9,7 +9,7 @@ from .models import Quote, Book
 
 
 class QuoteAdmin(admin.ModelAdmin):
-    list_display = ('date')
+    list_display = ('date',)
 
 admin.site.register(Quote, QuoteAdmin)
 admin.site.register(Book)
