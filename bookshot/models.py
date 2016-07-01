5f3b5d56bbf2b90211f7142a4f12012ebb1eb60a
eunjungchoi <im.your.energy@gmail.com>
2016-07-02 04:02:36 +0900

    ADD 'Book' class in models.py

--- bookshot/models.py
+++ bookshot/models.py
@@ -3,10 +3,9 @@ from django.contrib.auth.models import User
 
 
 
-
 class Quote(models.Model):
 	user = models.ForeignKey(User)
-	# book = models.ForeignKey('Book')
+	book = models.ForeignKey('Book', null=True, blank=True, default = None)
 	date = models.DateField()
 	quotation = models.TextField(max_length=1000)
 	photo = models.ImageField(upload_to='bookshot')
@@ -15,10 +14,9 @@ class Quote(models.Model):
 		return self.quotation
 
 
-
 class Book(models.Model):
 	title = models.CharField(max_length=30)
+	readers = models.ManyToManyField(User)
 	
 	def __str__(self):
-			return self.title
-
+		return self.title
