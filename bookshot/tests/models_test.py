7726a28b33d5487a9594f57ad0a5a0388a82d200
eunjungchoi <im.your.energy@gmail.com>
2016-07-06 02:32:20 +0900

    #16 TEST unittest for models.Quote (Full version)

--- bookshot/tests/models_test.py
+++ bookshot/tests/models_test.py
@@ -16,6 +16,7 @@ class BookTestCase(TestCase):
 
 
 
+# QuoteTest는 2가지 버전입니다. 위의 것은 좀더 심플하고요.
 
 class QuoteTestCase(TestCase):
 	def setUp(self):
@@ -29,8 +30,43 @@ class QuoteTestCase(TestCase):
 
 	def test_Quote_is_correctly_created(self):
 		quote = Quote.objects.get(quotation="봐, 나는 살아있어!")
+		initial_time = quote.updated_at
 
 		self.assertEqual(quote.user.username, 'jhk')
 		self.assertEqual(quote.book.title, '열혈강호')
 		self.assertEqual(quote.quotation, '봐, 나는 살아있어!')
+		self.assertEqual(bool(quote.created_at), True) 
+		self.assertEqual(bool(quote.updated_at), True)
+
+		quote.quotation="아냐 그렇지 않아!"
+		quote.save()
+
+		self.assertEqual(bool(initial_time == quote.updated_at), False) 
+
+
+
+
+## 아래는, 좀더 created_at, updated_at에 포커스를 두고, 
+## 시간이 좀더 '적당히'' '시간스럽게' 들어갔는지 확인하기 위한, 두번째 테스트코드입니다. 
+
+
+	def test_Quote_time_is_correctly_created(self):
+		quote = Quote.objects.get(quotation="봐, 나는 살아있어!")
+		initial_time = quote.updated_at
+
+		self.assertEqual(quote.user.username, 'jhk')
+		self.assertEqual(quote.book.title, '열혈강호')
+		self.assertEqual(quote.quotation, '봐, 나는 살아있어!')
+		self.assertEqual(bool(quote.created_at.day == quote.updated_at.day), True)
+		self.assertEqual(bool(quote.created_at.second == quote.updated_at.second), True)
+
+		quote.quotation = "아냐 그렇지 않아!"
+		quote.save()
+
+		self.assertEqual(bool(initial_time.microsecond == quote.updated_at.microsecond), False)
+
+
+
+
+
 
