0b0faa577c77677e45e16eebf725149193a65366
eunjungchoi <im.your.energy@gmail.com>
2016-07-06 01:48:42 +0900

    #16 TEST unittest for models.Quote

--- bookshot/tests/models_test.py
+++ bookshot/tests/models_test.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from bookshot.models import Book
+from django.contrib.auth.models import User
+
+from bookshot.models import Book, Quote
+
 
 
 class BookTestCase(TestCase):
@@ -10,3 +13,24 @@ class BookTestCase(TestCase):
         book = Book.objects.get(title="스토너")
 
         self.assertEqual(book.title, '스토너')
+
+
+
+
+class QuoteTestCase(TestCase):
+	def setUp(self):
+		user = User.objects.create_user('jhk', 'jh@gggmail.com', 'jhpassword')
+		book = Book.objects.create(title="열혈강호")
+		Quote.objects.create(
+			user=user,
+			book=book,
+			quotation="봐, 나는 살아있어!"
+		)
+
+	def test_Quote_is_correctly_created(self):
+		quote = Quote.objects.get(quotation="봐, 나는 살아있어!")
+
+		self.assertEqual(quote.user.username, 'jhk')
+		self.assertEqual(quote.book.title, '열혈강호')
+		self.assertEqual(quote.quotation, '봐, 나는 살아있어!')
+
