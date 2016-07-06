d1075d5142c388e222b49031cc669a96b47ce677
eunjungchoi <im.your.energy@gmail.com>
2016-07-07 00:55:13 +0900

    #17 ADD image resizing

---
+++ bookshot/tests/test_functions.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+from bookshot.views import calculate_size, resize_image
+import os
+
+
+class ImageTestCase(TestCase):
+
+	maxDiff = None
+
+	def setUp(self):
+		pass
+
+	def test_calculate_size(self):
+		width, height = calculate_size(320, 320)
+		self.assertEqual(width, 320)
+		self.assertEqual(height, 320)
+
+		width, height = calculate_size(640, 640)
+		self.assertEqual(width, 640)
+		self.assertEqual(height, 640)
+
+		width, height = calculate_size(1280, 1280)
+		self.assertEqual(width, 640)
+		self.assertEqual(height, 640)
+
+		width, height = calculate_size(850, 567)
+		self.assertEqual(width, 640)
+		self.assertEqual(height, 426)
+
+
+	def test_Image_is_correctly_resized(self):
+		image_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data/ritualcoffee3.jpg")
+		resize_image_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data/ritualcoffee3_resize.jpg")
+
+		resized_image = resize_image(image_filename, resize_image_filename)
+
+		self.assertIsNotNone(resized_image)
+		self.assertEqual(resized_image.width, 640)
--- bookshot/views/add_photo.py
+++ bookshot/views/add_photo.py
@@ -6,7 +6,40 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from datetime import date
+from PIL import Image
 from bookshot.models import *
+
+def calculate_size(width, height):
+	max_width, max_height = (640, 640)
+
+	image_ratio = width / float(height)
+
+	if width < max_width and height < max_height:
+		new_width = width
+		new_height = height
+	elif width > height:
+		new_width = max_width
+		new_height = max_height / image_ratio 
+	else: 
+		new_height = max_height
+		new_width  = max_width / image_ratio
+
+	return int(new_width), int(new_height)
+
+
+def resize_image(file_path, resized_file_path):
+	from PIL import Image
+	import os
+
+	image = Image.open(file_path)
+
+	image_width, image_height = image.size
+	new_width, new_height = calculate_size(image_width, image_height)
+
+	resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)	
+	resized_image.save(resized_file_path)
+
+	return resized_image
 
 
 @login_required
@@ -26,6 +59,8 @@ def add(request):
 		photo=request.FILES['photo'],
 		quotation=request.POST['quotation'],
 	)
+
+	resize_image(q.photo.path, q.photo.path)
 
 	return redirect(reverse('index'))
 
