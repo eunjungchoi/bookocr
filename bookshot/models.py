7f7683005ef3bd9d1367eb617430687add97736e
eunjungchoi <im.your.energy@gmail.com>
2016-07-10 05:29:05 +0900

    ADD isbn, cover_url, json field on Book

--- bookshot/models.py
+++ bookshot/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from jsonfield import JSONField
 
 
 class Quote(models.Model):
@@ -46,7 +47,7 @@ class Quote(models.Model):
 		resized_image.save(resized_file_path)
 
 		return resized_image
-	
+
 
 	def save(self, *args, **kwargs):
 		#
@@ -64,7 +65,49 @@ class Quote(models.Model):
 
 class Book(models.Model):
 	title = models.CharField(max_length=30)
+	isbn13 = models.CharField(max_length=13, null=False, blank=True)
+	cover_url = models.URLField(max_length=300, null=True, blank=True)
+	_raw_response = JSONField(null=True, blank=True, default=None)
+
 	readers = models.ManyToManyField(User)
-	
+
+	# http://code.activestate.com/recipes/498104-isbn-13-converter/
+	@staticmethod
+	def check_digit_13th(isbn):
+		assert len(isbn) == 12
+		sum = 0
+		for i in range(len(isbn)):
+			c = int(isbn[i])
+			if i % 2:
+				w = 3
+			else: 
+				w = 1
+			sum += w * c
+		r = 10 - (sum % 10)
+		if r == 10: 
+			return '0'
+		else:
+			return str(r)
+
+
+	@staticmethod
+	def convert_10_to_13(isbn):
+		assert len(isbn) == 10
+		prefix = '978' + isbn[:-1]
+		check = Book.check_digit_13th(prefix)
+		return prefix + check
+
+
+	def save(self, *args, **kwargs):
+		import re
+
+		if len(self.isbn13) ==10:
+			self.isbn13 = Book.convert_10_to_13(self.isbn13)
+
+		self.isbn13 = re.sub('[-]', '', self.isbn13)
+
+		return super(Book, self).save(*args, **kwargs)
+
+
 	def __str__(self):
 		return self.title
