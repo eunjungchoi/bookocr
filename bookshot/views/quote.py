1c7f3d49c7cd108def2684e90c8f32502d2994f3
김장환 Kim <jangxyz@hail.local>
2016-07-09 18:46:03 +0900

    ADD ocr url and templates
    - `GET  /books/:book_id/quotes/:quote_id/recognize` renders image, croppable
    - `POST /books/:book_id/quotes/:quote_id/ocr` updates quotation with crop info

--- bookshot/views/quote.py
+++ bookshot/views/quote.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
+
 from datetime import date
 from PIL import Image
 from bookshot.models import *
@@ -45,8 +46,7 @@ def resize_image(file_path, resized_file_path):
 @login_required
 def add(request):
 	book, created = Book.objects.get_or_create(
-		title=request.POST['book-title']
-	)
+		title=request.POST['book-title'])
 	
 	q = Quote.objects.create(
 		user=request.user,
@@ -55,7 +55,43 @@ def add(request):
 		quotation=request.POST['quotation'],
 	)
 
+	#
 	resize_image(q.photo.path, q.photo.path)
+
+	#return redirect(reverse('index'))
+	return redirect(reverse('ocr_quote', kwargs={"book_id": book.id, "quote_id": q.id}))
+
+
+@login_required
+def ocr_new(request, book_id, quote_id):
+	book  = get_object_or_404(Book, pk=book_id)
+	quote = get_object_or_404(Quote, pk=quote_id)
+
+	context = {
+		'book' : book,
+		'quote': quote
+	}
+
+	return render(request, 'quote/crop.html', context)
+
+@login_required
+def ocr_update(request, book_id, quote_id):
+	book  = get_object_or_404(Book, pk=book_id)
+	quote = get_object_or_404(Quote, pk=quote_id)
+
+	#
+	q_text    = request.POST['quotation']
+	crop_rect = {
+		'x': request.POST['crop-x'],
+		'y': request.POST['crop-y'],
+		'w': request.POST['crop-w'],
+		'h': request.POST['crop-h'],
+	}
+
+	quote.quotation = q_text
+	#quote._crop_info = crop_rect
+	#quote._ocr_raw_response = request.POST['ocr_raw_response']
+	#quote.save()
 
 	return redirect(reverse('index'))
 
@@ -66,7 +102,7 @@ def new(request):
 	recent_books = ('살인자의 기억법', '스토너')
 
 	context = {
-		recent_books: recent_books,
+		'recent_books': recent_books,
 	}
 	return render(request, 'quote/new.html', context)
 
