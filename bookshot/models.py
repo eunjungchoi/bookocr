af6379471246b0ddba477d1d09940265161198b0
eunjungchoi <im.your.energy@gmail.com>
2016-07-11 15:06:49 +0900

    ADD django json-field

--- bookshot/models.py
+++ bookshot/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from jsonfield import JSONField
+from django.contrib.postgres.fields import JSONField
 
 
 class Quote(models.Model):
