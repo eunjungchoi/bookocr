66a3849ba705959a0e5f3101b6abe53c93a2074e
Kim, Jang-hwan <janghwan@gmail.com>
2016-07-14 13:31:09 +0900

    TEST isbn13 should be falsey if not set
    - currently Book.isbn13 is set to "0000000000000" by default,
      but is difficult to check whether isbn is properly set or not.
    - it "should be set to falsey if not given"
    - this is a follow up of #22

--- bookshot/tests/test_isbn_book.py
+++ bookshot/tests/test_isbn_book.py
@@ -2,31 +2,21 @@ from django.test import TestCase
 from bookshot.models import Book, Quote
 
 
-
 class BookISBN13TestCase(TestCase):
-	def setUp(self):
-		self.book = Book.objects.create(title="우리 본성의 선한 천사", isbn13='9788983716897')
+	def test_no_isbn_is_set_to_falsey(self):
+		book = Book.objects.create(title="우리 본성의 선한 천사")
+		self.assertFalse(not not book.isbn13)
 
-
+class BookISBN13ConversionTestCase(TestCase):
+	
 	def test_return_same_when_isbn13(self):
-		self.assertEqual(self.book.isbn13, "9788983716897")
-
-
-
-class BookISBN10TestCase(TestCase):
-	def setUp(self):
-		self.book = Book.objects.create(title="스토너", isbn13='1590171993')
-
+		book = Book.objects.create(title="우리 본성의 선한 천사", isbn13='9788983716897')
+		self.assertEqual(book.isbn13, "9788983716897")
 
 	def test_convert_to_isbn13_when_isbn10(self):
-		self.assertEqual(self.book.isbn13, "9781590171998")
-
-
-
-class BookISBNfilterTestCase(TestCase):
-	def setUp(self):
-		self.book = Book.objects.create(title="반쪼가리 자작", isbn13='978-89374-62412')
-
+		book = Book.objects.create(title="스토너", isbn13='1590171993')
+		self.assertEqual(book.isbn13, "9781590171998")
 
 	def test_remove_dash_from_isbn13(self):
-		self.assertEqual(self.book.isbn13, "9788937462412")
+		book = Book.objects.create(title="반쪼가리 자작", isbn13='978-89374-62412')
+		self.assertEqual(book.isbn13, "9788937462412")
