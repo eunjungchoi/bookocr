bca11639c5bbca94785803fe89b34caba8d3c6f7
Kim, Jang-hwan <janghwan@gmail.com>
2016-07-22 15:56:33 +0900

    ADD try more colorful test runner by default

--- bookocr/test/runner.py
+++ bookocr/test/runner.py
@@ -1,17 +1,27 @@
-from django.test.runner import DiscoverRunner # default
+import os
+
+from django.test.runner import DiscoverRunner # default runner
 
 try:
-	from django_nose import NoseTestSuiteRunner as Runner
-	class BookOCRTestRunner(Runner):
+	# try using nose
+	from django_nose import NoseTestSuiteRunner
+	class BookOCRTestRunner(NoseTestSuiteRunner):
 		pass
+	# try rednose
+	try:
+		import rednose
+		os.environ['NOSE_REDNOSE'] = '1'
+	except ImportError: pass
+
 except ImportError:
+	# try using ColourRunnerMixin
 	print('django_nose not installed, trying ColourRunnerMixin')
 	try:
 		from colour_runner.django_runner import ColourRunnerMixin
-
 		class BookOCRTestRunner(ColourRunnerMixin, DiscoverRunner):
 			pass
 	except:
+		# fallback to default runner
 		print('ColourRunnerMixin not installed, reverting to default')
 		class BookOCRTestRunner(DiscoverRunner):
 			pass
