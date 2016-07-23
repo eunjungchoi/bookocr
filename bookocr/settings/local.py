971ea94af6180bd05588b619daf8b0c9dd6bcb59
Kim, Jang-hwan <janghwan@gmail.com>
2016-07-24 01:13:01 +0900

    ADD SQL logging for local settings
    - see https://docs.djangoproject.com/en/1.9/topics/logging/
      https://coderwall.com/p/uzhyca/quickly-setup-sql-query-logging-django
    
    - this is part of #30

--- bookocr/settings/local.py
+++ bookocr/settings/local.py
@@ -3,7 +3,25 @@
 from bookocr.settings.base import *
 
 DEBUG = True
+
 INSTALLED_APPS += (
     #'debug_toolbar', # and other apps for local development
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
 
