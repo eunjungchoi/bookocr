cfade044ebeb676abddf50830e5fd2075d870e98
Kim, Jang-hwan <janghwan@gmail.com>
2016-07-30 03:22:12 +0900

    SETTINGS let local.py read from .env file
    - read from .env file if exist,
    - unless NOENV=1 is configured
    - you can change filename with ENV=
    - contents in locals.py is written AFTER the load.
    
    this is done to:
    - for easier running on local environment (#42)

--- bookocr/settings/local.py
+++ bookocr/settings/local.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
 
+from django.conf import settings
+from bookocr.settings import base
 from bookocr.settings.base import *
+
+# try reading local .env
+no_env = os.getenv('NOENV')
+env_filename = os.getenv('ENV', '.env')
+if not no_env and os.path.exists(env_filename):
+	print('reading local "%s" file..' % (env_filename))
+	lines  = (line for line in open('.env'))
+	lines  = (line.strip() for line in lines)
+	lines  = (line for line in lines if line)
+	tuples = (line.split('=', 1) for line in lines)
+	for (key, value) in tuples:
+		locals()[key] = value
+
 
 DEBUG = True
 
