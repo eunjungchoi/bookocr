2d94579bd8ebba1b11572e4ed8872e34ab35babd
Eun Jung Choi <im.your.energy@gmail.com>
2016-08-05 23:45:45 +0900

    FIX redirect to http only when method == get

--- bookocr/http_middleware.py
+++ bookocr/http_middleware.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect
+import logging
+logger = logging.getLogger(__name__)
 
 class HttpRedirectMiddleware(object):
     def process_request(self, request):
-        if request.scheme == 'https':
+        if request.scheme == 'https' and request.method == "GET": #POST일때는 redirect를 하면 안됨.
             return redirect('http://{0}{1}'.format(request.META['HTTP_HOST'], request.path))
         return None
